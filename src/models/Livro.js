b92c2adbd9e24b786f69710783ddfba4caf67956
Guilherme Caxéro <caxeropl@gmail.com>
2025-08-02 03:11:38 -0300

    feat: Adicionando validações no schema de livro

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -4,9 +4,23 @@ import { autorSchema } from "./Autor.js"
 const livroSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
     title: { type: String, required: [true, "Título obrigatório"] },
-    editora: { type: String },
+    editora: { 
+        type: String,
+        enum: {
+            values: ["Wizard", "Jambo"],
+            message: "A editora {VALUE} não é valida!"
+        }
+     },
     preco: { type: Number },
-    paginas: { type: Number },
+    paginas: { 
+        type: Number,
+        validate: {
+            validator: (valor) => {
+                return valor >= 10 && valor <= 5000;
+            },
+            message: "O número de páginas deve ser entre 10 a 5000. Valor inserido {VALUE}"
+        }
+     },
     autor: {type: autorSchema, required: [true, "Para cadastrar um livro é necessário um autor!"]}
 }, {versionKey: false});
 
